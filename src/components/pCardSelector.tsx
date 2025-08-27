551c6a681baee762e7d9ff9d94ab173624528f32
jbillote <jbillote@users.noreply.github.com>
2025-08-27 12:08:55 -0700

    plan icons

--- src/components/pCardSelector.tsx
+++ src/components/pCardSelector.tsx
@@ -4,6 +4,12 @@ import { PrismaClient } from "@/generated/prisma"
 import { typeFromDBString } from "@/lib/cardTypes"
 import { rarityFromString } from "@/lib/rarity"
 
+import Image from "next/image"
+import FreePlan from "../../public/icon_plan_common.webp"
+import SensePlan from "../../public/icon_plan_plan1.webp"
+import LogicPlan from "../../public/icon_plan_plan2.webp"
+import AnomalyPlan from "../../public/icon_plan_plan3.webp"
+
 async function loadCards() {
     const prisma = new PrismaClient()
     const cards = await prisma.pCard.findMany({
@@ -44,7 +50,26 @@ export async function PCardSelector() {
             <div className="h-[5%]">
                 Filters
                 <div>
-                    Free Sense Logic Anomaly
+                    <Image
+                        src={FreePlan}
+                        alt="FREE"
+                        className="size-48px"
+                    />
+                    <Image
+                        src={SensePlan}
+                        alt="FREE"
+                        className="size-48px"
+                    />
+                    <Image
+                        src={LogicPlan}
+                        alt="FREE"
+                        className="size-48px"
+                    />
+                    <Image
+                        src={AnomalyPlan}
+                        alt="FREE"
+                        className="size-48px"
+                    />
                 </div>
                 <div>
                     Characters
