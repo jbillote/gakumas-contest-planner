78d8d4c147fc0c055c8500d10e7d009e2e078fbc
jbillote <jbillote@gmail.com>
2025-09-03 16:41:33 -0700

    update dialog and add customizable cards

--- src/components/pCardSelector.tsx
+++ src/components/pCardSelector.tsx
@@ -1,6 +1,11 @@
-import { PCardIcon } from "./pCardIcon"
-import { PrismaClient } from "@/generated/prisma"
+"use client"
 
+import { PCard } from "@/generated/prisma"
+import { PIdol, PIdols } from "@/lib/pIdols"
+import { Plan } from "@/generated/prisma"
+import { PCardIcon } from "./pCardIcon"
+
+import { loadCardsFromDB } from "@/lib/fetchCards"
 import { typeFromDBString } from "@/lib/cardTypes"
 import { rarityFromString } from "@/lib/rarity"
 
@@ -22,24 +27,20 @@ import SDMisuzu from "../../public/sd_icons/img_sd_hmsz_face-00.webp"
 import SDUme from "../../public/sd_icons/img_sd_hume_face-00.webp"
 import SDRinami from "../../public/sd_icons/img_sd_hrnm_face-00.webp"
 
-async function loadCards() {
-    const prisma = new PrismaClient()
-    const cards = await prisma.pCard.findMany({
-        select: {
-            id: true,
-            filename: true,
-            name: true,
-            type: true,
-            plan: true,
-            rarity: true,
-            customCharacter: true
-        }
-    })
-    return cards
-}
+import { use, useEffect, useState, useTransition } from "react"
 
-export async function PCardSelector() {
-    const cards = await loadCards()
+export function PCardSelector() {
+    const [currentPlan, setCurrentPlan] = useState(Plan.LOGIC)
+    const [currentCharacter, setCurrentCharacter] = useState(PIdols.Sena as PIdol)
+    const [cards, setCards] = useState([] as PCard[])
+
+    useEffect(() => {
+        (async () => {
+            const cards = await loadCardsFromDB(currentPlan)
+            setCards(cards)
+        })()
+    }, [])
+
     return (
         <>
             <div className="justify-center align-center overflow-auto flex-1">
@@ -48,7 +49,7 @@ export async function PCardSelector() {
                         cards.map((card) => (
                             <PCardIcon
                                 pCard={{
-                                    id: `${card.filename}${card.customCharacter ? '-jsna' : ''}`,
+                                    id: `${card.filename}${card.customCharacter ? currentCharacter : ''}`,
                                     enhanced: false,
                                     type: typeFromDBString(card.type),
                                     rarity: rarityFromString(card.rarity)
@@ -89,61 +90,73 @@ export async function PCardSelector() {
                         src={SDSaki}
                         alt="花海咲季"
                         className="size-8 cursor-pointer hover:bg-gray-500"
+                        onClick={() => { setCurrentCharacter(PIdols.Saki) }}
                     />
                     <Image
                         src={SDTemari}
                         alt="月村手毬"
                         className="size-8 cursor-pointer hover:bg-gray-500"
+                        onClick={() => { setCurrentCharacter(PIdols.Temari) }}
                     />
                     <Image
                         src={SDKotone}
                         alt="藤田ことね"
                         className="size-8 cursor-pointer hover:bg-gray-500"
+                        onClick={() => { setCurrentCharacter(PIdols.Kotone) }}
                     />
                     <Image
                         src={SDMao}
                         alt="有村麻央"
                         className="size-8 cursor-pointer hover:bg-gray-500"
+                        onClick={() => { setCurrentCharacter(PIdols.Mao) }}
                     />
                     <Image
                         src={SDLilja}
                         alt="葛城リーリヤ"
                         className="size-8 cursor-pointer hover:bg-gray-500"
+                        onClick={() => { setCurrentCharacter(PIdols.Lilja) }}
                     />
                     <Image
                         src={SDChina}
                         alt="倉本千奈"
                         className="size-8 cursor-pointer hover:bg-gray-500"
+                        onClick={() => { setCurrentCharacter(PIdols.China) }}
                     />
                     <Image
                         src={SDSumika}
                         alt="紫雲清夏"
                         className="size-8 cursor-pointer hover:bg-gray-500"
+                        onClick={() => { setCurrentCharacter(PIdols.Sumika) }}
                     />
                     <Image
                         src={SDHiro}
                         alt="篠澤広"
                         className="size-8 cursor-pointer hover:bg-gray-500"
+                        onClick={() => { setCurrentCharacter(PIdols.Hiro) }}
                     />
                     <Image
                         src={SDSena}
                         alt="十王星南"
                         className="size-8 cursor-pointer hover:bg-gray-500"
+                        onClick={() => { setCurrentCharacter(PIdols.Sena) }}
                     />
                     <Image
                         src={SDMisuzu}
                         alt="秦谷美鈴"
                         className="size-8 cursor-pointer hover:bg-gray-500"
+                        onClick={() => { setCurrentCharacter(PIdols.Misuzu) }}
                     />
                     <Image
                         src={SDUme}
                         alt="花海佑芽"
                         className="size-8 cursor-pointer hover:bg-gray-500"
+                        onClick={() => { setCurrentCharacter(PIdols.Ume) }}
                     />
                     <Image
                         src={SDRinami}
                         alt="姫崎莉波"
                         className="size-8 cursor-pointer hover:bg-gray-500"
+                        onClick={() => { setCurrentCharacter(PIdols.Rinami) }}
                     />
                 </div>
             </div>
